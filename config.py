6e91df5e5c1b0e437889c67cfb050faad3ebcbf1
7小果 <2545284108@qq.com>
2022-11-27 19:41:05 +0800

    更改number_negatives -> 4

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
         self.follow_in_user_path = "./data/gowalla/sensor.csv"
         self.embedding_size = 32 #嵌入层的维度设置为32维
         self.epoch = 25#30
-        self.num_negatives = 6
+        self.num_negatives = 4
         self.batch_size = 128 #貌似用128效果更好
         # self.lr = [0.000005, 0.000001, 0.0000005]
         self.lr = [0.01, 0.01, 0.001] #效果还不错
